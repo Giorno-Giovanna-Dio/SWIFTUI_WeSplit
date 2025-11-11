9ef545b18fc04ee5710139bc4390a2ed16a8850f
Giorno-Giovanna-Dio <dcetpn@gmail.com>
2025-11-12 06:53:22 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by David Chung on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
